6cf8236199ea6664eec5db99c7c2463d32bf7814
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2023-09-26 13:29:35 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,22 @@
 /* eslint-disable no-plusplus */
 import GameState from './GameState';
 
-function runGoblin() {
-  let nextPosition = -1;
-  while (nextPosition === GameState.currentPosition || nextPosition === -1) {
-    nextPosition = Math.round(Math.random() * 15);
+function randomMinMaxNot(min, max, not) {
+  let result = Math.round(Math.random() * (max - min) + min);
+  while (result === not) {
+    result = Math.round(Math.random() * (max - min) + min);
   }
-  const gamePad = document.getElementById('gamePad');
-  const goblin = document.getElementById('goblin');
-  const newPlace = gamePad.childNodes[nextPosition];
-  newPlace.appendChild(goblin);
-  goblin.classList.remove('invisible');
-  GameState.currentPosition = nextPosition;
+  return result;
+}
+
+function runGoblin() {
+  const board = Array.from(document.querySelectorAll('.myCell'));
+  const activeCell = board.find((el) => el.classList.contains('target'));
+  const index = board.indexOf(activeCell);
+  activeCell.classList.remove('target');
+  board[randomMinMaxNot(0, 15, index)].classList.add('target');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const AutoAddChild = (node, typ) => {
-    const newElement = document.createElement(typ);
-    node.appendChild(newElement);
-    return newElement;
-  };
-  const goblin = AutoAddChild(document.getElementById('workdiv'), 'div');
-  goblin.id = 'goblin';
-  goblin.classList.add('goblin');
-  goblin.classList.add('invisible');
-  // const goblinPic = AutoAddChild(goblin, 'img');
-  // goblinPic.src = './img/goblin.png';
-  // const gamePad = document.getElementById('gamePad');
-  // let htmlVar = '';
-  // for (let y = 0; y < 16; y++) {
-  //  htmlVar += '<div class="myCell"></div>';
-  // }
-  // gamePad.innerHTML = htmlVar;
-  GameState.currentPosition = -1;
   setInterval(() => { runGoblin(); }, 1000);
-  // eslint-disable-next-line no-console
-  // console.log('it works!');
 });
